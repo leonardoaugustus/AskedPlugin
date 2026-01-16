762aa00909551fc0b14d1d39b3fbba65e6ecf0d7
Leonardo Augustus <leoaugusto45@gmail.com>
2026-01-16 19:39:39 -0300

    new version

--- js/asked.js
+++ js/asked.js
@@ -2,8 +2,8 @@
  * Asked jQuery Plugin
  * 
  *
- * @updated November 14, 2022
- * @version 1.0
+ * @updated April 13, 2025
+ * @version 2.0
  *
  * @author Leonardo Augustus - https://linktr.ee/unitbox
  * @copyright (c) 2022 Unitbox - https://www.unitbox.com.br
@@ -20,8 +20,12 @@
         defaults = {
             data: [],
             textButtonSave: 'Save &raquo;',
+            classButtonSave: 'btn btn-primary',
+            classTitleQuiz: 'h1',
+            titleQuiz: true,
+            destroy: false,
             events: {
-                // onStartQuiz: function (options) { },
+                onStartQuiz: function (options) { },
                 onCompleteQuiz: function (options) { }
             }
         }
@@ -32,25 +36,32 @@
             method: {
 
                 renderRadio: function (question, count_question) {
-
                     var html = "";
-                    html += "<div class=\"form-group mb-3\" id=\"question_" + question.id + "\">";
-                    html += "<h5 class=\"fw-normal mb-2\">" + count_question + ") " + question.title + "</h5>";
+                    html += "<div class=\"form-group mb-3 px-3\" id=\"question_" + question.id + "\">";
+                    html += "<label class=\"h5 fs-0\">" + count_question + ") " + question.title + "</label>";
 
                     question.items.forEach(item => {
-                        var checked = item.selected == false ? '' : 'checked';
-                        var required = ((question.required == true && question.answered == false) ? ' required="required" ' : '');
-
-                        html += "<div class=\"form-check m-0\">";
-                        html += "<input class=\"form-check-input\" type=\"radio\" name=\"question_" + question.id + "\" id=\"item_" + item.id + "\" value=\"" + item.value +
-                            "\" " + checked + required + ">";
-                        html += "<label class=\"fw-medium \" for=\"item_" + item.id + "\">" + item.value + "</label>";
-                        html += "</div>";
+                        // Se o item for do tipo "texto", renderiza o input com label e preenche com o valor salvo (value_answer)
+                        if (item.type && item.type.toLowerCase() === "texto") {
+                            html += "<div class=\"mb-3\">";
+                            html += "<label for=\"question_" + question.id + "_item_" + item.id + "_input\">" + item.value + "</label>";
+                            html += "<input type=\"text\" class=\"form-control\" data-question-type=\"radio\" data-question-item-id=\"" + item.id + "\" name=\"question_" + question.id + "_item_" + item.id + "_input\" id=\"question_" + question.id + "_item_" + item.id + "_input\" placeholder=\"" + item.value + "\" value=\"" + (item.saved_value ? item.saved_value : "") + "\">";
+                            html += "</div>";
+                        } else {
+                            // Renderiza o radio normalmente para os demais itens
+                            var checked = item.selected ? 'checked' : '';
+                            var required = ((question.required == true && question.answered == false) ? ' required="required" ' : '');
+                            html += "<div class=\"form-check m-0\">";
+                            html += "<input class=\"form-check-input\" type=\"radio\" name=\"question_" + question.id + "\" id=\"item_" + item.id + "\" value=\"" + item.value + "\" " + checked + required + ">";
+                            html += "<label class=\"fw-medium\" for=\"item_" + item.id + "\">" + item.value + "</label>";
+                            html += "</div>";
+                        }
                     });
                     html += "</div>";
 
                     return html;
                 },
+
 
                 renderText: function (question, count_question) {
 
@@ -60,8 +71,8 @@
                     }
 
                     var html = '';
-                    html += "<div class=\"form-group mb-3\" id=\"question_" + question.id + "\" data-rule=\"" + rule + "\">";
-                    html += "<h5 class=\"fw-normal mb-2\" for=\"question_" + question.id + "\">" + count_question + ") " + question.title + "</h5>";
+                    html += "<div class=\"form-group mb-3 px-3\" id=\"question_" + question.id + "\" data-rule=\"" + rule + "\">";
+                    html += "<span class=\"h5 fs-0\" for=\"question_" + question.id + "\">" + count_question + ") " + question.title + "</span>";
                     html += internal.method.renderInputType(question, question.value_answer);
                     html += "</div>";
 
@@ -72,10 +83,11 @@
 
                     var html = "";
                     var required = (question.obrigatorio == true ? ' required="required" ' : '');
+                    var regra = (question.rule == "editable" ? 'editable' : '');
 
-                    html += "<div class=\"form-group mb-3\">";
-                    html += "<h5 class=\"fw-normal mb-2\" for=\"question_" + question.id + "\">" + count_question + ") " + question.title + "</h5>";
-                    html += "<textarea type=\"textarea\" class=\"form-control\" name=\"question_" + question.id + "\" rows=\"5\" cols=\"70\"" + required + ">" + question.value_answer + "</textarea>";
+                    html += "<div class=\"form-group mb-3 px-3\">";
+                    html += "<span class=\"h5 fs-0\" for=\"question_" + question.id + "\">" + count_question + ") " + question.title + "</span>";
+                    html += "<textarea type=\"textarea\" class=\"form-control "+regra+"\" name=\"question_" + question.id + "\" rows=\"5\" cols=\"70\"" + required + ">" + (question.value_answer == null ? '' : question.value_answer) + "</textarea>";
                     html += "</div>";
 
                     return html;
@@ -86,9 +98,8 @@
                     var required = (question.required == true ? ' required="required" ' : '');
 
                     switch (question.rule) {
-                        case "numeric":
-                            input += "<input type=\"numeric\" min=\"0\" class=\"form-control\" name=\"item_" + question.id + "\" value=\"" + value_answer +
-                                "\" inputmode=\"numeric\" pattern=\"[0-9]*\" " + required + "/>";
+                        case "number":
+                            input += "<input type=\"number\" class=\"form-control\" name=\"question_" + question.id + "\" value=\"" + value_answer + "\"" + required + ">";
                             break;
                         case "telefone":
                             input += "<input type=\"text\" class=\"form-control phone\" name=\"question_" + question.id + "\" value=\"" + value_answer + "\"" + required + ">";
@@ -112,29 +123,29 @@
 
                 renderCheckbox: function (question, count_question) {
                     var html = '';
-                    html += "<div class=\"form-group mb-3\" id=\"question_" + question.id + "\">";
-                    html += "<h5 class=\"fw-normal mb-2\">" + count_question + ") " + question.title + "</h5>";
+                    html += "<div class=\"form-group mb-3 px-3\" id=\"question_" + question.id + "\">";
+                    html += "<span class=\"h5 fs-0\">" + count_question + ") " + question.title + "</span>";
 
                     var required = '';
+                    // Checa se nenhum dos itens está marcado
                     var answered = $.map(question.items, function (n, i) {
-                        if (n.selected == true) {
+                        if (n.selected === true) {
                             return n.selected;
                         }
                     });
+                    // Se não houver nenhum item marcado e a questão for obrigatória, adiciona o atributo required
+                    if (!answered.length && question.required === true) {
+                        required = ' required="required" ';
+                    }
 
                     question.items.forEach(item => {
-                        var checked = (item.selected == true ? ' checked ' : '');
-                        if (answered == false) {
-                            if (question.required == true) {
-                                required = ' required="required" ';
-                            }
-                        }
-
+                        var checked = (item.selected === true ? ' checked ' : '');
+                        // Monte o input com os atributos devidamente separados
                         html += "<div class=\"form-check\">";
-                        html += "<input class=\"form-check-input\" type=\"checkbox\" name=\"question_" +
-                            question.id + "\" id=\"item_" + item.id + "\" value=\"" + item.value + "\"" +
-                            "onclick=\"changeCheckboxRequired('" + question.id + "', '" + question.required + "');" + "\"" + required + "";
-                        html += checked + ">";
+                        html += "<input class=\"form-check-input\" type=\"checkbox\" name=\"question_" + question.id + "\" id=\"item_" + item.id + "\" value=\"" + item.value + "\" " +
+                            "data-question-item-id=\"" + item.id + "\" " +
+                            "onclick=\"changeCheckboxRequired('" + question.id + "', '" + question.required + "');\" " +
+                            required + " " + checked + ">";
                         html += "<label class=\"form-check-label\" for=\"item_" + item.id + "\">" + item.value + "</label>";
                         html += "</div>";
                     });
@@ -142,6 +153,7 @@
 
                     return html;
                 }
+
             }
         };
 
@@ -150,33 +162,32 @@
             setupQuiz: function () {
 
                 var count_quiz_id = 0;
-
                 plugin.config.data.forEach(quiz => {
 
                     if (quiz.questions.length >= 1) {
 
                         var html = "";
-                        if (quiz.title != "" && quiz.title != null && typeof quiz.title != 'undefined') {
-                            html += "<h2>" + quiz.title + "</h2>";
+
+                        if (plugin.config.titleQuiz && quiz.title) {
+                            html += "<h2 class=\"" + plugin.config.classTitleQuiz + "\">" + quiz.title + "</h2>";
                         }
 
-                        var quiz_id = 0;
-                        if (quiz.id == "" || quiz.id == null || typeof quiz.id == 'undefined') {
+                        var quiz_id = quiz.id;
+                        if (!quiz_id) {
                             count_quiz_id++;
                             quiz_id = count_quiz_id;
-                        } else {
-                            quiz_id = quiz.id;
                         }
 
-                        html += "<form action=\"" + quiz.route + "\" class=\"mb-3 g-3\" method=\"POST\" name=\"form_quiz_" + "_" + quiz_id + "\"";
-                        html += "onsubmit=\"return submitQuiz('" + quiz_id + "');\" quiz-required=" + quiz.required + "";
+                        // Mantém o nome do form com dois underscores para compatibilidade com o seletor em askedResult
+                        html += "<form action=\"" + quiz.route + "\" class=\"mb-3 g-3 form_quiz\" method=\"POST\" name=\"form_quiz_" + "_" + quiz_id + "\" data-quiz-id=\"" + quiz_id + "\" ";
+                        html += "onsubmit=\"return submitQuiz('" + quiz_id + "');\" quiz-required=\"" + quiz.required + "\"";
                         html += ">";
 
                         var count_question = 0;
                         var order_question = 0;
                         quiz.questions.forEach(question => {
 
-                            if (question.order == "" || question.order == null || typeof question.order == 'undefined') {
+                            if (!question.order) {
                                 count_question++;
                                 order_question = count_question;
                             } else {
@@ -189,7 +200,7 @@
                             } else if (question.type == "textarea") {
                                 html += internal.method.renderTextArea(question, order_question);
 
-                            } else if (question.type == "text") {
+                            } else if (question.type == "input") {
                                 html += internal.method.renderText(question, order_question);
 
                             } else if (question.type == "checkbox") {
@@ -197,42 +208,44 @@
                             }
                         });
 
-                        html += "<button type=\"submit\" class=\"btn btn-sm btn-primary\">" + plugin.config.textButtonSave + "</button>";
+                        html += "<button type=\"submit\" class=\"" + plugin.config.classButtonSave + "\">" + plugin.config.textButtonSave + "</button>";
                         html += "</form>";
-                        // html += '<input type="text" id="quiz_complete_' + quiz.questionario_id + '" value="">';
                         $element.append(html);
                     }
                 });
+
             },
 
             askedResult: function (quiz_id) {
-
                 var form_quiz = $('form[name="form_quiz_' + "_" + quiz_id + '"]');
-                let $inputs = $("input, textarea, select", form_quiz);
-                var answers = [];
+                var answers = $("input, textarea, select", form_quiz).map(function () {
+                    var $input = $(this);
+                    var parts = $input.attr('name').split('_');
+                    var question_id = parts[1];
+                    var question_type = $input.data('question-type') || $input.attr('type');
+                    var question_item_id = "";
 
-                $.each($inputs, function (index, value) {
-
-                    var question_id = $(value).attr('name').replace('question_', '');
-                    var question_type = $(value).attr('type');
-                    var question_item_id = (question_type != "text" && question_type != 'textarea' ? $(value).attr('id') : '').replace('item_', '');
-
-                    if ($(value).is('input:checkbox, input:radio, input:text, textarea')) {
-
-                        var answered = (question_type == "checkbox" || question_type == "radio" ? $(value).is(':checked') : true);
-
-                        answers.push({
-                            "question_id": question_id,
-                            "question_type": question_type,
-                            "question_item_id": question_item_id,
-                            "value": $(value).val(),
-                            "question_rule": (question_type == "text" ? $("#question_" + question_id).attr('data-rule') : ''),
-                            "answered": answered
-                        });
+                    if (question_type === "radio" || question_type === "checkbox") {
+                        question_item_id = $input.data('question-item-id') || ($input.attr('id') ? $input.attr('id').replace('item_', '') : '');
                     }
-                });
+
+                    var answered = (question_type === "radio" || question_type === "checkbox")
+                        ? ($input.attr('type') === "text" ? $input.val().trim() !== "" : $input.is(':checked'))
+                        : true;
+
+                    return {
+                        question_id: question_id,
+                        question_type: question_type,
+                        question_item_id: question_item_id,
+                        value: $input.val(),
+                        question_rule: (question_type === "input" ? $("#question_" + question_id).attr('data-rule') : ''),
+                        answered: answered
+                    };
+                }).get();
                 return answers;
-            },
+            }
+
+
         };
 
         plugin.init = function () {
@@ -256,9 +269,9 @@
             submitQuiz = function (quiz_id) {
                 event.preventDefault();
                 var askeds = plugin.method.askedResult(quiz_id);
-
-                if (plugin.config.events && plugin.config.events.onCompleteQuiz) {
-                    plugin.config.events.onCompleteQuiz.apply(null, [askeds]);
+                if (plugin.config.events && typeof plugin.config.events.onCompleteQuiz === 'function') {
+                    // Agora passamos como segundo argumento o quiz_id
+                    plugin.config.events.onCompleteQuiz(askeds, quiz_id);
                 }
             }
 
@@ -274,10 +287,18 @@
     $.fn.asked = function (options) {
 
         return this.each(function () {
-            if (undefined === $(this).data('asked')) {
-                var plugin = new $.asked(this, options);
-                $(this).data('asked', plugin);
+            $(this).removeData('asked');
+            $(this).html("");
+
+            if (undefined !== $(this).data('asked')) {
+                if (options.destroy) {
+                    $(this).removeData('asked');
+                    $(this).html("");
+                }
             }
+
+            var plugin = new $.asked(this, options);
+            $(this).data('asked', plugin);
         });
     };
 }(jQuery));
